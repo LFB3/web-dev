b30921d21a06cb1fd655e100b58a66070ed9bcac
LFB3 <LFB3@outlook.de>
2026-03-08 21:19:08 +0100

    ram zeug

--- src/index.js
+++ src/index.js
@@ -10,25 +10,25 @@ app.use(express.json());
 
 
 
-app.get('/api/connectiontest', (req, res) => {
+app.get('/api/connectiontest', async (req, res) => {
     res.json({
         status: "success"
     });
 });
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
     res.sendFile(path.join(__dirname, 'web', 'index.html'));
 });
-app.get('/style.css', (req, res) => {
+app.get('/style.css', async (req, res) => {
     res.sendFile(path.join(__dirname, 'web', 'style.css'));
 });
-app.get('/colors.css', (req, res) => {
+app.get('/colors.css', async(req, res) => {
     res.sendFile(path.join(__dirname, 'web', 'colors.css'));
 });
-app.get('/script.js', (req, res) => {
+app.get('/script.js', async (req, res) => {
     res.sendFile(path.join(__dirname, 'web', 'script.js'));
 });
-app.get('/api/cpu', (req, res) => {
+app.get('/api/cpu', async (req, res) => {
     osutils.cpu.usage().then(result => {
     if (result.success) {
         res.json({
@@ -43,22 +43,33 @@ app.get('/api/cpu', (req, res) => {
 });
 });
 
-app.get('/api/ram', (req, res) => {
-    osutils.memory.info(result => {
-        if (result.success) {
-            console.log(result.data)
-            res.json({
-                status: "success",
-                usage: result.data
+app.get('/api/ram', async (req, res) => {
+    try {
+        const memInfo = await osutils.memory.info();
+        if (memInfo.success) {
+            data = ({
+                usagePercentage: memInfo.data.usagePercentage,
+                total: memInfo.data.total.bytes,
+                used: memInfo.data.used.bytes
+            })
+            res.status(200).json({
+                success: true,
+                data: data
             });
         } else {
-            res.json({
-                status: "failed"
+            res.status(500).json({
+                success: false,
+                message: "Error, coud not get memory info"
             });
         }
-    });
+    } catch (error) {
+        console.error("Error while trying to access Memory Info:", error);
+        res.status(500).json({
+            success: false,
+            error: error.message
+        });
+    }
 });
-
 app.listen(3000, () => {
     console.log('Server startet')
 })
--- src/web/script.js
+++ src/web/script.js
@@ -1,20 +1,54 @@
-const usage = document.getElementById('usage');
-
-const loop = setInterval(async () => {
+const cpu = document.getElementById('cpu');
+async function fetchCpuUsage() {
     try {
         const response = await fetch('http://127.0.0.1:3000/api/cpu');
-        
+
         if (!response.ok) {
-            throw new Error('Netzwerk-Antwort war nicht ok');
+            throw new Error('Network Error Code was not 200');
         }
 
-        const data = await response.json(); 
-        
+        const data = await response.json();
         rounded = Math.round(data.usage * 100) / 100;
-        usage.textContent = `${rounded}%`;
-        
+        cpu.textContent = `${rounded}%`;
     } catch (error) {
-        console.error('Fehler beim Abrufen der Daten:', error);
-        usage.textContent = "Fehler!";
+        console.error('Eroor while trying to access Data:', error);
+        cpu.textContent = 'Error!';
     }
-}, 1000);
+}
+function memoryFormatter(bytes) {
+    if (bytes >= 1099511627776) {
+        return(`${(bytes / 1099511627776)}TB`)
+    }
+    if (bytes >= 1073741824) {
+        return(`${(bytes / 1073741824)}GB`)
+    }
+    if (bytes >= 1048576) {
+        return(`${(bytes / 1048576)}KB`)
+    }
+    if (bytes >= 1024) {
+        return(`${(bytes / 1024)}KB`)
+    }
+}
+async function fetchRamUsage() {
+    try {
+        const response = await fetch('http://127.0.0.1:3000/api/ram');
+
+        if (!response.ok) {
+            throw new Error('Network Error Code was not 200');
+        }
+
+        const data = await response.json();
+        ramUsed.textContent = memoryFormatter(data.used);
+        ramTotal.textContent = memoryFormatter(data.total);
+        ramPercentage.textContent = memoryFormatter(data.usagePercentage);
+    } catch (error) {
+        console.error('Eroor while trying to access Data:', error);
+        cpu.textContent = 'Error!';
+    }
+}
+fetchCpuUsage()
+fetchRamUsage()
+const loop = setInterval(async () => {
+    fetchCpuUsage()
+    fetchRamUsage()
+}, 6000);
